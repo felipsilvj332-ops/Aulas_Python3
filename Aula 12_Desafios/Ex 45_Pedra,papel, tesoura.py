775d04aba791f9d0b6cd8806f88ef77e38af1cf6
Felipe.Tec. <felipsilvj332@gmail.com>
2026-05-14 16:36:27 -0300

    Atualizando exércicio 44 e 45

---
+++ Aula 12_Desafios/Ex 45_Pedra,papel, tesoura.py
@@ -0,0 +1,16 @@
+## Crie um programa que faça o computador jogar Jokenpô com o usuário.
+
+## O programa deve permitir que o usuário escolha uma das opções:
+
+## Pedra
+## Papel
+## Tesoura
+
+## Em seguida, o computador deve escolher uma opção aleatória.
+## Depois, o programa deve comparar as escolhas e informar o resultado:
+
+## Usuário venceu
+## Computador venceu
+## Empate
+
+## O programa também deve exibir na tela a escolha do usuário e a escolha do computador.
