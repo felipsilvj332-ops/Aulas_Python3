b88af7311ec8c0a972d636a137c5a039fa679060
Felipe.Tec. <felipsilvj332@gmail.com>
2026-05-20 07:18:34 -0300

    Revisão
    Adição de campo de resumo, correção do calculo de juros mensais

--- Aula 12_Desafios/Ex 36_aprovação de emprestimo.py
+++ Aula 12_Desafios/Ex 36_aprovação de emprestimo.py
@@ -4,20 +4,28 @@
 # Se ultrapassar o emprestimo é negado
 
 print("Emprestimos certamentecaros")
-nome = input("Sejá Bem vindo a sua avaliação de crédito!\nPara começarmos qual seu nome?")
+nome = input("Sejá Bem vindo a sua avaliação de crédito!\nPara começarmos qual seu nome? ")
 print(f"Muito prazer {nome}! precisamos de mais detalhes sobre você, então informe:")
 salario = float(input("Salario liquido: R$"))
 parcelas = int(input("Quantidade de parcelas mênsais pretendidas?: "))
 valcasa = float(input(f"\nPara prosseguir informe: \nO valor da casa desejado: R$"))
 
-taxajuros = 0.15
+valano = parcelas / 12
+taxajuros = 0.015
+limite = salario * 0.30
 totalparcelas = parcelas
-valor_juros = valcasa + (valcasa * taxajuros)
-prestmensal = valor_juros / parcelas
+valor_cjuros = valcasa + (valcasa * taxajuros)
+prestmensal = valor_cjuros / parcelas
+#print(f"O limite é R${limite:.2f}\n")
+#print(f"Parcelas de R${prestmensal:.2f}\n")
+#print(f"o valor será de R${prestmensal:.2f}\n")
 
-print(f"o valor será de R${prestmensal}")
-
-if prestmensal>salario:
+if prestmensal>limite:
     print("Seu emprestimo foi reprovado!")
 else:
-    print("Seu emprestimo foi aprovado")
+    print("Seu emprestimo foi aprovado, parabens!\n")
+    print("RESUMO DO ACORDO: ")
+    print(f"Valor solicitado: R${valcasa}\n")
+    print(f"Anos a pagar: {valano} anos.\n")
+    print(f"Parcelas mensais de R${prestmensal:.2f}")
+    print("Custo total: ", prestmensal * parcelas)
